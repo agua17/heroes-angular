abc2737368b8f0888858d862da1e9c28ce1bb17b
agua17 <aguassantasnunez@gmail.com>
2024-04-08 10:31:18 +0200

    boton_back_click

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,12 +1,21 @@
-import { Component,Input,Output } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component,Input,Output,OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.css']
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
+
+  constructor( private location : Location) {}
+
+  ngOnInit(): void {}
+
+  onClick(){
+    this.location.back();
+  }
 
 
 
